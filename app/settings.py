c32db0f429d23589872270c35c8478196c40433e
Denys Vusyk <denis06699@gmail.com>
2024-03-20 13:17:04 +0200

    Slug conventer for product

--- app/settings.py
+++ app/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 
 from django.conf.global_settings import MEDIA_ROOT, MEDIA_URL
 
+from django.http import HttpResponseNotFound
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
